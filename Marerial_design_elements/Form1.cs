b2a06a40b34bd8e58154600f2073f931e84b4138
ManoelRIos <manoel.rios@cfc.org.br>
2021-10-08 13:26:08 -0300

    Adicionando função de capturar vídeo

--- Marerial_design_elements/Form1.cs
+++ Marerial_design_elements/Form1.cs
@@ -10,16 +10,44 @@ using System.Windows.Forms;
 using MaterialSkin;
 using MaterialSkin.Controls;
 using ComponentFactory.Krypton.Toolkit;
-
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
 namespace Marerial_design_elements
 {
     public partial class Form1 : KryptonForm
     {
+        #region variables
+        private Capture videoCapture = null;
+        private Image<Bgr, Byte> currentFrame = null;
+        Mat frame = new Mat();
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
                                  
                    
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            videoCapture = new Capture();
+            videoCapture.ImageGrabbed += ProcessFrame;
+            videoCapture.Start();
+
+
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            //Capturar vídeo
+            videoCapture.Retrieve(frame, 0);
+            currentFrame = frame.ToImage<Bgr, Byte>().Resize(picCapture.Width, picCapture.Height, Inter.Cubic);
+
+            //renderizar a captura do vídeo dentro da Picture box picCapture
+            picCapture.Image = currentFrame.Bitmap;
         }
     }
 }
